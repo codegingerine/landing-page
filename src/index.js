10d0880222e0af481338826f216858ac5a4a3859
codegingerine <urskosik@gmail.com>
2021-03-30 01:44:41 +0200

    feature: add mobile menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import "./style/style.scss";
 import { scrollToAnchor } from "./utils/helpers.js";
 
-const menuLinks = document.querySelectorAll(
+const menuSrollLinks = document.querySelectorAll(
   ".menu-item--scrolling .menu-item_link"
 );
+const menuLinks = document.querySelectorAll(".menu-item_link");
+const menuToggler = document.querySelector(".menu-toggler");
+const menuList = document.querySelector(".menu_links");
+const backdrop = document.querySelector("#backdrop");
+const body = document.body;
+
+const toggleItemsArr = [...menuLinks, menuList, menuToggler, backdrop, body];
+const closeItemsArr = [...menuLinks, backdrop];
+
+const toggleMobileMenu = () => {
+  toggleItemsArr.forEach((link) => link.classList.toggle("open"));
+};
+
+const closeMobileMenu = () => {
+  toggleItemsArr.forEach(
+    (link) => link.classList.contains("open") && link.classList.remove("open")
+  );
+};
 
 // menu events
-menuLinks.forEach((link) => {
+menuSrollLinks.forEach((link) => {
   link.addEventListener("click", scrollToAnchor);
+});
+
+menuToggler.addEventListener("click", toggleMobileMenu);
+
+closeItemsArr.forEach((link) => {
+  link.addEventListener("click", closeMobileMenu);
 });
