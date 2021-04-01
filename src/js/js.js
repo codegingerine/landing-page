da2b0182b5deac39684d6fd43707a6e4d6ccf40b
codegingerine <urskosik@gmail.com>
2021-04-01 21:17:45 +0200

    fix: replace just-validate with validate.js

--- src/js/js.js
+++ src/js/js.js
@@ -1,5 +1,6 @@
-require("just-validate");
 import { scrollToAnchor } from "../utils/helpers.js";
+const validate = require("validate.js");
+const _ = require("underscore");
 
 const menuSrollLinks = document.querySelectorAll(
   ".menu-item--scrolling .menu-item_link"
@@ -34,65 +35,121 @@ closeItemsArr.forEach((link) => {
   link.addEventListener("click", closeMobileMenu);
 });
 
-// log form values to be submitted
-/*
-const firstNameEl = document.querySelector('[name="first-name"]');
-const lastNameEl = document.querySelector('[name="last-name"]');
-const phoneEl = document.querySelector('[name="phone"]');
-const selectEl = document.querySelector(".form_item-select");
-const submitEl = document.querySelector("#btn-submit");
+// form validation with validate.js
+(function () {
+  validate.validators.presence.options = { message: "field can't be empty" };
+  var constraints = {
+    "first-name": {
+      presence: true,
+      length: {
+        minimum: 3,
+      },
+      format: {
+        pattern: "[a-z]+",
+        flags: "i",
+        message: "can only contain letters a-z",
+      },
+    },
+    "last-name": {
+      presence: true,
+      length: {
+        minimum: 3,
+      },
+      format: {
+        pattern: "[a-z]+",
+        flags: "i",
+        message: "can only contain letters a-z",
+      },
+    },
+    phone: {
+      presence: true,
+      length: {
+        minimum: 6,
+      },
+      format: {
+        pattern: "[0-9]+",
+        flags: "i",
+        message: "can only contain numbers 0-9",
+      },
+    },
+    services: {
+      presence: true,
+    },
+  };
 
-submitEl.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("Form values to submit: ", {
-    "first name": firstNameEl.value,
-    "last name": lastNameEl.value, 
-    "phone": phoneEl.value, 
-    "service": selectEl.value, 
+  var form = document.querySelector(".js-form");
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    handleFormSubmit(form);
   });
-});
-*/
 
-// just-validate lib -- contact form validation
-new window.JustValidate(".js-form", {
-  rules: {
-    firstName: {
-      required: true,
-      minLength: 3,
-    },
-    lastName: {
-      required: true,
-      minLength: 3,
-    },
-    phone: {
-      required: true,
-    },
-    select: {
-      required: true,
-    },
-  },
+  var inputs = document.querySelectorAll("input, select");
+  for (var i = 0; i < inputs.length; ++i) {
+    inputs.item(i).addEventListener("change", function (ev) {
+      var errors = validate(form, constraints) || {};
+      showErrorsForInput(this, errors[this.name]);
+    });
+  }
 
-  messages: {
-    firstName: {
-      required: "This field is required!",
-      minLength: "Min. 3 characters are required.",
-    },
-    lastName: {
-      required: "This field is required!",
-      minLength: "Min. 3 characters are required",
-    },
-    phone: {
-      required: "This field is required!",
-    },
-    select: {
-      required: "Required field. Choose from the list.",
-    },
-  },
+  function handleFormSubmit(form, input) {
+    var errors = validate(form, constraints);
+    showErrors(form, errors || {});
+    if (!errors) {
+      showSuccess();
+    }
+  }
 
-  colorWrong: "#D52047",
-  focusWrongField: true,
+  function showErrors(form, errors) {
+    _.each(
+      form.querySelectorAll("input[name], select[name]"),
+      function (input) {
+        showErrorsForInput(input, errors && errors[input.name]);
+      }
+    );
+  }
 
-  submitHandler: function (form, values) {
-    console.log("Form values to submit: ", values)
-  },
-});
+  function showErrorsForInput(input, errors) {
+    var formGroup = closestParent(input.parentNode, "form_item"),
+      messages = formGroup.querySelector(".message");
+    resetFormGroup(formGroup);
+    if (errors) {
+      formGroup.classList.add("has-error");
+      _.each(errors, function (error) {
+        addError(messages, error);
+      });
+    } else {
+      formGroup.classList.add("has-success");
+    }
+  }
+
+  function closestParent(child, className) {
+    if (!child || child == document) {
+      return null;
+    }
+    if (child.classList.contains(className)) {
+      return child;
+    } else {
+      return closestParent(child.parentNode, className);
+    }
+  }
+
+  function resetFormGroup(formGroup) {
+    formGroup.classList.remove("has-error");
+    formGroup.classList.remove("has-success");
+    _.each(formGroup.querySelectorAll(".error"), function (el) {
+      el.parentNode.removeChild(el);
+    });
+  }
+
+  function addError(messages, error) {
+    var block = document.createElement("p");
+    block.classList.add("error");
+    block.classList.add("text-danger");
+    block.innerText = error;
+    messages.appendChild(block);
+  }
+
+  function showSuccess() {
+    alert( `Success!!!!! \n Form values to be submitted: \n ${JSON.stringify(validate.collectFormValues(form), null, 2)}`)
+  }
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -18,15 +18,6 @@ module.exports = {
     poll: 2000,
     ignored: "**/node_modules",
   },
-  resolve: {
-    alias: {
-      "just-validate": path.resolve(
-        __dirname,
-        "node_modules",
-        "just-validate/dist/js/just-validate.min.js"
-      ),
-    },
-  },
   module: {
     rules: [
       {
