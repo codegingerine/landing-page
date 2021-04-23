27ac52429086ca974d581d4931b65284030c2311
codegingerine <urskosik@gmail.com>
2021-04-23 11:33:54 +0200

    chore: refactor menu js events

--- src/js/js.js
+++ src/js/js.js
@@ -12,49 +12,32 @@ const menuLinks = document.querySelectorAll(".menu-item_link");
 const menuToggler = document.querySelector(".menu-toggler");
 const menuList = document.querySelector(".menu_links");
 const backdrop = document.querySelector("#backdrop");
+const scrollToTopButton = document.getElementById("js-top");
 const body = document.body;
+const rootElement = document.documentElement;
 
 const toggleItemsArr = [...menuLinks, menuList, menuToggler, backdrop, body];
 const closeItemsArr = [...menuLinks, backdrop];
 
-// menu sticky on scroll
-document.addEventListener("scroll", () => {
-  const top = document.documentElement.scrollTop || document.body.scrollTop;
-  top > 88 ? menu.classList.add("sticky") : menu.classList.remove("sticky");
-});
-
-// menu link events
-const handleMenuLinkEvents = () => {
-  scrollToAnchor(menuSrollLinks);
-  setActiveLink(menuSrollLinks, "active");
-};
-handleMenuLinkEvents();
-
-// mobile menu
+// toggle mobile menu
 const toggleMobileMenu = () => {
-  toggleItemsArr.forEach((link) => link.classList.toggle("open"));
-};
-
-const closeMobileMenu = () => {
-  toggleItemsArr.forEach(
-    (link) => link.classList.contains("open") && link.classList.remove("open")
-  );
-};
-
-const handleMenuToggler = () =>
-  menuToggler.addEventListener("click", toggleMobileMenu);
-handleMenuToggler();
-
-const handleCloseMobileMenu = () =>
-  closeItemsArr.forEach((link) => {
-    link.addEventListener("click", closeMobileMenu);
+  menuToggler.addEventListener("click", () => {
+    toggleItemsArr.forEach((link) => link.classList.toggle("open"));
   });
-handleCloseMobileMenu();
+};
 
-// scroll to top button
-const scrollToTopButton = document.getElementById("js-top");
-const rootElement = document.documentElement;
+// close mobile menu
+const closeMobileMenu = () =>
+  closeItemsArr.forEach((link) => {
+    link.addEventListener("click", () => {
+      toggleItemsArr.forEach(
+        (link) =>
+          link.classList.contains("open") && link.classList.remove("open")
+      );
+    });
+  });
 
+// show scroll to top button
 const showScrollTopBtn = () => {
   const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
 
@@ -63,7 +46,28 @@ const showScrollTopBtn = () => {
     : scrollToTopButton.classList.remove("show");
 };
 
-document.addEventListener("scroll", showScrollTopBtn);
+// add menu sticky on scroll
+const addStickyMenu = () => {
+  const top = document.documentElement.scrollTop || document.body.scrollTop;
+  top > 88 ? menu.classList.add("sticky") : menu.classList.remove("sticky");
+};
+
+// === fire menu events
+const handleMenuEvents = () => {
+  scrollToAnchor(menuSrollLinks);
+  setActiveLink(menuSrollLinks, "active");
+  toggleMobileMenu();
+  closeMobileMenu();
+};
+handleMenuEvents();
+
+// === fire menu events on scroll
+document.addEventListener("scroll", () => {
+  addStickyMenu();
+  showScrollTopBtn();
+});
+
+// === fire scroll to top event on button click
 scrollToTopButton.addEventListener("click", (e) => {
   e.preventDefault();
   scrollToTop(0);
